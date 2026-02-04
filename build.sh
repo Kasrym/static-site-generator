70138f60a49fd89e23c93a04699b3e8d1d3939f5
Kasrym <theseraphkasrym@gmail.com>
2026-02-03 19:26:59 -0600

    Bug fix

--- build.sh
+++ build.sh
@@ -1,2 +1,1 @@
-# Replace REPO_NAME with your actual repository name on GitHub
 python3 src/main.py "/static-site-generator/"
--- src/gencontent.py
+++ src/gencontent.py
@@ -25,7 +25,7 @@ def generate_page(from_path, template_path, dest_path, basepath):
     
     final_html = template.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
     final_html = final_html.replace('href="/', f'href="{basepath}')
-    final_html = final_html.replace('src="', f'src="{basepath}')
+    final_html = final_html.replace('src="/', f'src="{basepath}')
 
     # Write output and create directories if needed
     dest_dir_path = os.path.dirname(dest_path)
